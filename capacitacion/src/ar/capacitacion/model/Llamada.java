4cccdb98a39021abe4282d65cd2515ba7243421a
dan <horrisdev@gmail.com>
2013-05-21 20:42:08 -0300

    cambio de paquete para identificar clases del modelo de dominio del problema

---
+++ capacitacion/src/ar/capacitacion/model/Llamada.java
@@ -0,0 +1,31 @@
+package ar.capacitacion.model;
+
+import java.util.*;
+
+/**
+ * @author dan
+ *
+ */
+public class Llamada {
+	
+	private Date inicio;
+	
+	private Date fin;
+
+	public Date getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Date inicio) {
+		this.inicio = inicio;
+	}
+
+	public Date getFin() {
+		return fin;
+	}
+
+	public void setFin(Date fin) {
+		this.fin = fin;
+	}
+
+}
---
+++ capacitacion/src/ar/capacitacion/model/PlanTelefono.java
@@ -0,0 +1,17 @@
+package ar.capacitacion.model;
+
+import java.math.BigDecimal;
+
+/**
+ * @author dan
+ *
+ */
+public class PlanTelefono {
+	
+	BigDecimal costo;
+	
+	Integer minutosPlan;
+	
+	
+
+}
