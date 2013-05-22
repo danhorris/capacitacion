89cd7f6e8d4d1f3a5fd7fe356e99023793a9904d
dan <horrisdev@gmail.com>
2013-05-21 22:38:23 -0300

    clases para test

---
+++ capacitacion/test/ar/capacitacion/test/TelefoniaTest.java
@@ -0,0 +1,14 @@
+package ar.capacitacion.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TelefoniaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
