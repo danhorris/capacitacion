f356c4e508e4f6e797b870bbd468865fd9a706d9
danhorris <horrisdev@gmail.com>
2013-05-25 19:10:13 -0300

    test para el calculador de primos

--- capacitacion/test/ar/capacitacion/threads/CalculadorPrimosTest.java
+++ capacitacion/test/ar/capacitacion/threads/CalculadorPrimosTest.java
@@ -8,6 +8,17 @@ public class CalculadorPrimosTest {
 	public void test() {
 		CalculadorPrimos calculator = new CalculadorPrimos();
 		calculator.start();
+		calculator.start();
+		 
+		 try {
+			Thread.sleep(CalculadorPrimos.TEN_SECONDS);
+		} catch (InterruptedException e) {
+		
+			e.printStackTrace();
+		}
+
+		 calculator.finished = true;
+
 	}
 
 }
