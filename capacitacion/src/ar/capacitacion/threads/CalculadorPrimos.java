4b9f7114f453811a912ed3345c9b9b02968f6144
danhorris <horrisdev@gmail.com>
2013-05-25 20:31:26 -0300

    Configuro desde afuera el tiempo

--- capacitacion/src/ar/capacitacion/threads/CalculadorPrimos.java
+++ capacitacion/src/ar/capacitacion/threads/CalculadorPrimos.java
@@ -1,34 +1,37 @@
 package ar.capacitacion.threads;
 
 /**
- * Calcula numeros primos en 10 segundos. 
+ * Calcula numeros primos en un tiempo dado.
+ * 
  * @author dan
  * 
  */
 public class CalculadorPrimos extends Thread {
 
-	public static final int MAX_PRIMES = 1000000;
-	public static final int TEN_SECONDS = 10000;
+	private static final int MAX_PRIMES = 1000000;
 
-	public volatile boolean finished = false;
+	/* variable compartida */
+	public boolean finished = false;
 
 	public void run() {
-
 		int[] primes = new int[MAX_PRIMES];
 		int count = 0;
+
 		for (int i = 2; count < MAX_PRIMES; i++) {
 
-			// Check to see if the timer has expired
 			if (finished) {
 				break;
 			}
+
 			boolean prime = true;
+
 			for (int j = 0; j < count; j++) {
 				if (i % primes[j] == 0) {
 					prime = false;
 					break;
 				}
 			}
+
 			if (prime) {
 				primes[count++] = i;
 				System.out.println("Found prime: " + i);
