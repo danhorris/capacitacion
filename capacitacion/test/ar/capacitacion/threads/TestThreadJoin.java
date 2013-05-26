4f77987595eab4169f36d51dc79049103d877326
danhorris <horrisdev@gmail.com>
2013-05-25 23:05:56 -0300

    join de hilos

---
+++ capacitacion/test/ar/capacitacion/threads/TestThreadJoin.java
@@ -0,0 +1,38 @@
+package ar.capacitacion.threads;
+
+import org.junit.Test;
+
+public class TestThreadJoin {
+
+	public class Hilo1 extends Thread {
+		@Override
+		public void run() {
+			System.out.println("A");
+			System.out.println("B");
+		}
+	}
+
+	public class Hilo2 extends Thread {
+		@Override
+		public void run() {
+			System.out.println("1");
+			System.out.println("2");
+		}
+	}
+
+	@Test
+	public void test() {
+		Hilo1 hilo1 = new Hilo1();
+		hilo1.start();
+		Hilo2 hilo2 = new Hilo2();
+		hilo2.start();
+		
+		try {
+			hilo1.join();
+			hilo2.join();
+		} catch (InterruptedException e) {
+		}
+		
+	}
+	
+}
