0387ead139135791102f306a70837161a89afab3
dan <horrisdev@gmail.com>
2013-05-26 17:30:50 -0300

    tests Daemons

---
+++ capacitacion/test/ar/capacitacion/threads/TestThreadDaemon.java
@@ -0,0 +1,32 @@
+package ar.capacitacion.threads;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+public class TestThreadDaemon {
+
+	public class Hilo1 extends Thread {
+		@Override
+		public void run() {
+			while (true) {
+				System.out.println(new Date());
+			}
+
+		}
+	}
+
+	@Test
+	public void testTimer() {
+		Hilo1 hilo1 = new Hilo1();
+		// Se hace un hilo daemon ANTES que se le de start()
+		hilo1.setDaemon(true);
+		hilo1.start();
+		try {
+			hilo1.join();
+		} catch (InterruptedException e) {
+			System.out.println("saliendo");
+		}
+	}
+
+}
