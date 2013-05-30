32d4285ea251405e7763dd0b80c028e40fdf12ed
dan <horrisdev@gmail.com>
2013-05-29 22:50:46 -0300

    elimino print stacktrace innecesarios

--- capacitacion/src/ar/capacitacion/archivos/LectorArchivos.java
+++ capacitacion/src/ar/capacitacion/archivos/LectorArchivos.java
@@ -31,7 +31,7 @@ public class LectorArchivos {
 			while ((linea = br.readLine()) != null)
 				lineasLeidas.add(linea);
 		} catch (Exception e) {
-			e.printStackTrace();
+
 		} finally {
 			// En el finally cerramos el fichero, para asegurarnos
 			// que se cierra tanto si todo va bien como si salta
@@ -41,7 +41,7 @@ public class LectorArchivos {
 					fr.close();
 				}
 			} catch (Exception e2) {
-				e2.printStackTrace();
+
 			}
 		}
 		return lineasLeidas;
