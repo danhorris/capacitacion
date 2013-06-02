7e8be405461d76f280dd651f23b4a94d07b14598
danhorris <horrisdev@gmail.com>
2013-06-02 14:11:21 -0300

    leer directorio desde una variable en archivo propiedades

---
+++ capacitacion/test/ar/capacitacion/utils/PropertiesTest.java
@@ -0,0 +1,22 @@
+package ar.capacitacion.utils;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.junit.Test;
+
+public class PropertiesTest {
+
+	@Test
+	public void testPropertiesFile() {
+		Properties properties = new Properties();
+		try {
+			properties.load(new FileInputStream("project.properties"));
+		} catch (Exception e) {
+		}
+		assertNotNull(properties.getProperty("dir.input"));
+		
+	}
+}
