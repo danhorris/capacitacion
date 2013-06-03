676bdc242d6b5e6a8619147b5a53360e52642a46
danhorris <horrisdev@gmail.com>
2013-06-02 21:12:09 -0300

    comparacion con linea vacia. Cierro buffer

--- capacitacion/src/ar/capacitacion/archivos/LectorArchivos.java
+++ capacitacion/src/ar/capacitacion/archivos/LectorArchivos.java
@@ -14,12 +14,15 @@ import java.util.List;
  * 
  */
 public class LectorArchivos {
-	
+
+	private static final String VACIO = "";
 	private File file;
 	private FileReader fileReader;
 	private BufferedReader buffer;
+	private String lineaLeida;
 
 	/**
+	 * 
 	 * @param pathFile
 	 */
 	public LectorArchivos(String pathFile) {
@@ -32,22 +35,27 @@ public class LectorArchivos {
 		}
 		buffer = new BufferedReader(fileReader);
 	}
-	
 
 	/**
 	 * Retorna las lineas de un archivo. TODO hay que cerrar archivo?
+	 * 
 	 * @return
 	 */
 	public List<String> leer() {
 		List<String> lineasLeidas = new ArrayList<String>();
 		try {
 			while (buffer.ready()) {
-				lineasLeidas.add(buffer.readLine());
+				lineaLeida = buffer.readLine();
+				if (!lineaLeida.equals(VACIO)){ 
+					lineasLeidas.add(lineaLeida);	
+				}
 			}
+			buffer.close();
 		} catch (IOException error) {
 			System.out.println(error.getMessage());
 		}
+
 		return lineasLeidas;
 	}
-	
+
 }
