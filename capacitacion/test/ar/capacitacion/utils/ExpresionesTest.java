4bda39ef9d28eb9c13f8512cb980e404f9fe3d90
dan <horrisdev@gmail.com>
2013-06-04 20:41:17 -0300

    mas test para expresiones regulares

--- capacitacion/test/ar/capacitacion/utils/ExpresionesTest.java
+++ capacitacion/test/ar/capacitacion/utils/ExpresionesTest.java
@@ -1,5 +1,6 @@
 package ar.capacitacion.utils;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -19,12 +20,24 @@ public class ExpresionesTest {
 		String prueba2 = "a";
 		assertTrue(prueba2.matches("."));
 
-		// Comienza con regex
-		String prueba3 = "regex1236";
-		assertTrue(prueba3.matches("^regex"));
+	}
 
-		// Termina con regex
-		String prueba4 = "1236regex";
-		assertTrue(prueba4.matches("regex$"));
+	@Test
+	public void testExpresionesRegulares2() {
+		// Contiene string
+		String prueba3 = "DijoHola2veces";
+		assertTrue(prueba3.matches(".*Hola.*"));
+
+		// edad valida de 1 a 999 - indico caracteres posibles y ocurrencias
+		String edadValida = "77";
+		assertTrue(edadValida.matches("[0-9]{1,3}"));
+		edadValida = "2";
+		assertTrue(edadValida.matches("[0-9]{1,3}"));
+		edadValida = "999";
+		assertTrue(edadValida.matches("[0-9]{1,3}"));
+		edadValida = "1000";
+		assertFalse(edadValida.matches("[0-9]{1,3}"));
+		edadValida = "dos";
+		assertFalse(edadValida.matches("[0-9]{1,3}"));
 	}
 }
