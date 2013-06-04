67e4c5dc103fa111b26a6b571942565265275b6e
dan <horrisdev@gmail.com>
2013-06-04 19:49:49 -0300

    expresiones regulares simples

--- capacitacion/test/ar/capacitacion/utils/ExpresionesTest.java
+++ capacitacion/test/ar/capacitacion/utils/ExpresionesTest.java
@@ -7,8 +7,16 @@ import org.junit.Test;
 public class ExpresionesTest {
 
 	@Test
-	public void testExpresionRegular() {
-		String prueba = "123ab";
-		assertTrue(prueba.matches("123ab"));
+	public void testExpresionRegularSimples() {
+		// prueba literal
+		String prueba = "Hola";
+		assertTrue(prueba.matches("Hola"));
+
+		String prueba1 = "Hola363";
+		assertTrue(prueba1.matches("Hola363"));
+
+		// rexexp "." un caracter
+		String prueba2 = "a";
+		assertTrue(prueba2.matches("."));
 	}
 }
