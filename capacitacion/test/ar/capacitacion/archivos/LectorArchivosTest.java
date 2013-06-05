557ba87cb1a279c3ec20dac4edeb57e1a7f843c8
dan <horrisdev@gmail.com>
2013-06-04 23:16:05 -0300

    leer archivo procesando con expresiones regulares

--- capacitacion/test/ar/capacitacion/archivos/LectorArchivosTest.java
+++ capacitacion/test/ar/capacitacion/archivos/LectorArchivosTest.java
@@ -1,5 +1,6 @@
 package ar.capacitacion.archivos;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 import java.util.List;
@@ -9,9 +10,23 @@ import org.junit.Test;
 public class LectorArchivosTest {
 
 	@Test
-	public void testPathArchivoCorrecto() {
+	public void testLeerLineasArchivo() {
 		LectorArchivos lector = new LectorArchivos("resources/archivo.txt");
 		List<String> lineas = lector.leer();
 		assertFalse(lineas.isEmpty());
+	}
+
+	@Test
+	public void testLeerLineasConRexExp() {
+		LectorArchivos lector = new LectorArchivos("resources/archivo.txt");
+
+		List<String> lineas = lector.leerFiltrandoPorRexExp(".*Sanchez.*");
+		assertEquals(lineas.size(), 1);
+
+		LectorArchivos lector2 = new LectorArchivos("resources/archivo.txt");
+
+		List<String> lineas2 = lector2
+				.leerFiltrandoPorRexExp(".*(Sanchez|Martinez).*");
+		assertEquals(lineas2.size(), 2);
 	}
 }
