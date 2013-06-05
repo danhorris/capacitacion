12208cae80c4b869e73be514a69e9b1c149d2a0c
dan <horrisdev@gmail.com>
2013-06-04 21:54:30 -0300

    test email con regexp

--- capacitacion/test/ar/capacitacion/utils/ExpresionesTest.java
+++ capacitacion/test/ar/capacitacion/utils/ExpresionesTest.java
@@ -28,7 +28,12 @@ public class ExpresionesTest {
 		String prueba3 = "DijoHola2veces";
 		assertTrue(prueba3.matches(".*Hola.*"));
 
-		// edad valida de 1 a 999 - indico caracteres posibles y ocurrencias
+		prueba3 = "Hola";
+		assertTrue(prueba3.matches(".*Hola.*"));
+
+		// edad valida de 1 a 999
+		// [0-9] indico numeros posibles
+		// y ocurrencias {desde,hasta}
 		String edadValida = "77";
 		assertTrue(edadValida.matches("[0-9]{1,3}"));
 		edadValida = "2";
@@ -45,11 +50,29 @@ public class ExpresionesTest {
 		edadValida = "0";
 		assertTrue(edadValida.matches("[0-9]{1,3}"));
 
-		// primer caracter entre 1-9 los siguientes dos opcionales ()? 0-9
+		// primer caracter entre 1-9
+		// los siguientes dos opcionales ()? 0-9
 		edadValida = "11";
 		assertTrue(edadValida.matches("[1-9]{1}([0-9]{1,2})?"));
 
 		edadInvalida = "09";
 		assertFalse(edadInvalida.matches("[1-9]{1}([0-9]{1,2})?"));
+	}
+
+	@Test
+	public void testEmail() {
+		// + indica una o mas ocurrencias
+		String emailDan = "danhorris@gmail.com";
+		assertTrue(emailDan.matches("[a-zA-Z0-9]+\\@[a-zA-Z0-9]+\\.[a-zA-Z]+"));
+
+		emailDan = "danhorris@gmail.";
+		assertFalse(emailDan.matches("[a-zA-Z0-9]+\\@[a-zA-Z0-9]+\\.[a-zA-Z]+"));
+
+		emailDan = "@gmail.com";
+		assertFalse(emailDan.matches("[a-zA-Z0-9]+\\@[a-zA-Z0-9]+\\.[a-zA-Z]+"));
+
+		emailDan = "danhorris@gmailcom";
+		assertFalse(emailDan.matches("[a-zA-Z0-9]+\\@[a-zA-Z0-9]+\\.[a-zA-Z]+"));
+
 	}
 }
