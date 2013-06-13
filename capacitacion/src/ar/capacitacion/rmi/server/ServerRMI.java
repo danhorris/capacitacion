785b91fd858686edf20f84812d9f0e20a627ae52
dan <horrisdev@gmail.com>
2013-06-13 20:30:37 -0300

    rename server, static initialization registry

--- capacitacion/src/ar/capacitacion/rmi/server/ServerRMI.java
+++ capacitacion/src/ar/capacitacion/rmi/server/ServerRMI.java
@@ -1,6 +1,9 @@
 package ar.capacitacion.rmi.server;
 
 import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 import ar.capacitacion.rmi.CalculatorImpl;
 import ar.capacitacion.rmi.ICalculator;
@@ -9,11 +12,20 @@ import ar.capacitacion.rmi.ICalculator;
  * @author dan
  * 
  */
-public class CalculatorServer {
+public class ServerRMI {
+
+	// Creo el registro en metodo static de manera de crearlo una sola vez
+	static {
+		try {
+			Registry registry = LocateRegistry.createRegistry(1099);
+		} catch (RemoteException e) {
+		}
+	}
 
 	public void start() {
 		try {
 			ICalculator c = new CalculatorImpl();
+
 			// rmi://localhost:port/application_name
 
 			Naming.rebind("Calculator", c);
