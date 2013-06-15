3a8c85b7235431e71bd999aea10ef310d4e67962
dan <horrisdev@gmail.com>
2013-06-15 14:36:34 -0300

    before y after for connection

--- capacitacion/test/ar/capacitacion/jdbc/H2JDBCTest.java
+++ capacitacion/test/ar/capacitacion/jdbc/H2JDBCTest.java
@@ -1,16 +1,43 @@
 package ar.capacitacion.jdbc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import junit.framework.Assert;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author dan
+ * 
+ */
 public class H2JDBCTest {
+
+	private Connection conn;
+
+	@Before
+	public void createConnection() {
+		conn = new H2Connection().getConnection();
+	}
+
+	@After
+	public void closeConnection() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Test
 	public void testConnection() {
-
-		Assert.assertNotNull(new H2Connection().getConnection());
-
+		Assert.assertNotNull(conn);
 	}
 
+	@Test
+	public void testStamentJDBC() {
+
+	}
 }
