f9095f65d627f6d2aa72b9763073f85ad820db40
danhorris <horrisdev@gmail.com>
2013-06-16 18:54:18 -0300

    adapt test because modify return

--- capacitacion/test/ar/capacitacion/jdbc/H2JDBCTest.java
+++ capacitacion/test/ar/capacitacion/jdbc/H2JDBCTest.java
@@ -47,7 +47,7 @@ public class H2JDBCTest {
 			//obtengo la primera tupla
 			resultSet.next();
 			//obtengo la primer columna y la tipo a int.
-			Assert.assertEquals(1, resultSet.getInt(1));
+			Assert.assertEquals(4, resultSet.getInt(1));
 			resultSet.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -57,11 +57,11 @@ public class H2JDBCTest {
 	@Test
 	public void testPreparedStatementInsert() {
 		try {
-			h2Acces.executePreparedStatementInsertTest("daniel", "danhorris@gmail.com", "primer insert");
+			Assert.assertEquals(1, h2Acces.executePreparedStatementInsertTest("daniel", "danhorris@gmail.com", "primer insert"));
 			
 			ResultSet resultSet =  h2Acces.executeStatment("Select count(*) from TEST");
 			resultSet.next();
-			Assert.assertEquals(2,resultSet.getInt(1));
+			Assert.assertEquals(5,resultSet.getInt(1));
 			resultSet.close();
 			
 		} catch (SQLException e) {
